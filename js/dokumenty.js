55e336256920a32fd8c6f13bbe01f7345788328b
SURFACEBOOK3\intro <petr.introvic@showmax.com>
2021-10-24 17:33:31 +0200

    Add invite for november 2021

--- js/dokumenty.js
+++ js/dokumenty.js
@@ -1,5 +1,9 @@
 // { date: "", name: "", note: "", size: "", file: ""},
 
+const documents2021 = [
+    { date: "24. 10. 2021", name: "Pozvánka na schůzi listopad 2021", note: "&nbsp;", size: "105 kB ", file: "pozvanka-2021-11-15.pdf"},
+];
+
 const documents2020 = [
     { date: "29. 6. 2020", name: "Zápis schůze č.19", note: "Nové představenstvo a kontrolní komise, odměnuy okna, výtah, střecha", size: "145 kB", file: "zapis-ze-schuze-BD-c18-dne-2019-10-21.pdf"},
     { date: "2. 6. 2020", name: "Pozvánka na schůzi červen 2020", note: "&nbsp;", size: "472 kB ", file: "pozvanka-2020-06-17.pdf"},
@@ -88,6 +92,7 @@ function writeYear(year, documents) {
 </div>");
 }
 
+writeYear(2021, documents2021);
 writeYear(2020, documents2020);
 writeYear(2019, documents2019);
 writeYear(2018, documents2018);
